09f7e5bdd8083464707129a5be86261a71f2443a
OrlandoHR <orlandojosehr1997@gmail.com>
2018-11-25 23:32:06 -0600

    Simbolo.java Agregado

--- ABC_Compilador/src/Compilador/Simbolo.java
+++ ABC_Compilador/src/Compilador/Simbolo.java
@@ -9,6 +9,53 @@ package Compilador;
  *
  * @author orlandojose
  */
-public class Simbolo {
-    
+public class Simbolo 
+{
+    public Object name;
+    public Object type;
+    public String ambito;
+    public String linea;
+
+    public Simbolo(Object name, Object type,String ambito, String linea) {
+        this.name = name;
+        this.type = type;
+        this.ambito = ambito;
+        this.linea = linea;
+    }
+
+    public Object getType() {
+        return type;
+    }
+
+    public void setType(Object type) {
+        this.type = type;
+    }
+    public Object getName() {
+        return name;
+    }
+
+    public void setName(Object name) {
+        this.name = name;
+    }
+
+    public String getAmbito() {
+        return ambito;
+    }
+
+    public void setAmbito(String ambito) {
+        this.ambito = ambito;
+    }
+
+    public String getLinea() {
+        return linea;
+    }
+
+    public void setLinea(String linea) {
+        this.linea = linea;
+    }
+
+    @Override
+    public String toString() {
+        return "Nombre = " + name + ", Tipo = " + type + ", Ambito = " + ambito + ","+ linea;
+    }
 }
